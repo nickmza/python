778ea3eebafff9b313cc1a8622920ab5f0fb18b6
nicmck <nicholas.mckenzie@mcb.mu>
2023-09-06 14:36:11 +0400

    Basic map loading working.

--- rover/rover.py
+++ rover/rover.py
@@ -2,19 +2,28 @@ import sys
 from enum import Enum
 from itertools import groupby
 
-
-def readFile():
-    input = sys.argv[1]
-    with open(input,"r") as file:
-        board = [list(line.strip()) for line in file]
-    return board
-
 def runner():   
     #board = readFile()
     pass
     
 if __name__ == runner() :
     runner()    
+
+def readFile():
+    input = "./rover/roverMap.txt" #sys.argv[1]
+    with open(input,"r") as file:
+        board = [list(line.strip()) for line in file]
+    return board
+
+
+class Grid():
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+def parseBoard(input: list) -> Grid:
+    grid = Grid(len(input[0]), len(input))
+    return grid
 
 
 
--- rover/rovertest.py
+++ rover/rovertest.py
@@ -2,7 +2,17 @@ import unittest
 import rover
 
 class TestRover(unittest.TestCase):        
-    pass
+    def test_board_parse(self):
+        inputFile = rover.readFile()
+        self.assertTrue(len(inputFile) == 10)
+    
+    def test_parse_board_dimension(self):
+        inputFile = rover.readFile()
+        grid = rover.parseBoard(inputFile)
+        self.assertTrue(grid.width == 20)
+        self.assertTrue(grid.height == 10)
+
+
 
 if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+    unittest.main()
